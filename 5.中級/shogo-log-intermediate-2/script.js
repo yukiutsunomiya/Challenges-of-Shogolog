fce6a438d67f3798df253760f1d0dc0fe96a4d8d
utsunomiyayuki <58559473+yukiutsunomiya@users.noreply.github.com>
2023-10-15 10:59:04 +0900

    中級パート2を作成致しました。

--- 5.中級/shogo-log-intermediate-2/script.js
+++ 5.中級/shogo-log-intermediate-2/script.js
@@ -22,10 +22,71 @@ const swiper = new Swiper('.swiper', {
   }
 });
 
+let group1 = document.getElementById("group1");
+let dd1 = document.getElementById("dd1");   //  ボタンを取得
+
+let changeElement1 = (el)=> {
+  el.classList.toggle('dd1');
+}
+
+group1.addEventListener('click', ()=> {
+  changeElement1(dd1);
+}, false);
+
+
+let group2 = document.getElementById("group2");
+let dd2 = document.getElementById("dd2");   //  ボタンを取得
+
+let changeElement2 = (el)=> {
+  el.classList.toggle('dd2');
+}
+
+group2.addEventListener('click', ()=> {
+  changeElement2(dd2);
+}, false);
+
+let group3 = document.getElementById("group3");
+let dd3 = document.getElementById("dd3");   //  ボタンを取得
+
+let changeElement3 = (el)=> {
+  el.classList.toggle('dd3');
+}
+
+group3.addEventListener('click', ()=> {
+  changeElement3(dd3);
+}, false);
+
+let group4 = document.getElementById("group4");
+const dd4 = document.getElementById("dd4");   //  ボタンを取得
+
+let changeElement4 = (el)=> {
+  el.classList.toggle('dd4');
+}
+
+group4.addEventListener('click', ()=> {
+  changeElement4(dd4);
+}, false);
+
+let group5 = document.getElementById("group5");
+let dd5 = document.getElementById("dd5");   //  ボタンを取得
+
+let changeElement5 = (el)=> {
+  el.classList.toggle('dd5');
+}
+
+group5.addEventListener('click', ()=> {
+  changeElement5(dd5);
+}, false);
+
+let mb_menu = document.getElementById("mb-menu");
 document.querySelector('.hamburger').addEventListener('click', function(){
   this.classList.toggle('active');
-  document.querySelector('.slide-menu').classList.toggle('active');
+  changeElement(mb_menu);
 })
+
+let changeElement = (el)=> {
+  el.classList.toggle('mb-menu-active');
+}
 
 
 
